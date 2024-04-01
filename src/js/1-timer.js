9083a930dfb33b5e23648ecdef6f131c6f0f8de9
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-04-01 23:25:13 +0300

    task 1 done? task 2 begin

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,3 @@
-console.log('Timer');
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
@@ -13,15 +11,38 @@ const value = timer.querySelectorAll('.value');
 
 let userSelectedDate;
 let timerInterval;
+setButtonState("disable");
+setBorder('#808080');
 
+// Функція для встановлення рамки коли таймер активний
+function setBorder(color) {
+    input.style.border = `1px solid ${color}`;
+}
 
+// Функція для встановлення стану кнопки
+function setButtonState(state) {
+    switch (state) {
+        case "disable":
+            startBtn.disabled = true;
+            startBtn.style.backgroundColor = "";
+            startBtn.style.color = "";
+            break;
+        case "normal":
+            startBtn.disabled = false;
+            startBtn.style.backgroundColor = "#4E75FF"; 
+            startBtn.style.color = "#fff"; 
+            break;
+        default:
+            break;
+    }
+}
+
+// Ініціалізація Flatpickr
 flatpickr("#datetime-picker", {
         enableTime: true,
         time_24hr: true,
         defaultDate: new Date(),
         minuteIncrement: 1,
-        // dateFormat: "Y-m-d",
-        // timeFormat: "H:i",
         onClose(selectedDates) {
             const selectedDate = selectedDates[0];
             const currentDate = new Date();
@@ -29,22 +50,51 @@ flatpickr("#datetime-picker", {
             if (selectedDate > currentDate) {
                 userSelectedDate = selectedDate;
                 startBtn.disabled = false;
+                setButtonState("normal");
+                setBorder('#4E75FF');
+                
             } else {
                 userSelectedDate = null;
                 startBtn.disabled = true;
-                window.alert("Please choose a date in the future");
+               iziToast.error({
+                    title: 'Error',
+                    message: 'Please choose a date in the future',
+                    messageColor: '#fff',
+                    titleColor: '#fff',
+                    backgroundColor: '#EF4040',
+               });
+                setBorder('#808080');
             }
         }
 });
     
+
+// Додавання обробника події при натисканні на кнопку старт
 startBtn.addEventListener('click', () => {
     if (userSelectedDate) {
         startBtn.disabled = true;
         input.disabled = true;
         timerInterval = setInterval(updateTimer, 1000);
+        setButtonState("disable");
     }
 })
 
+// Додавання обробника події для стану hover кнопки
+startBtn.addEventListener('mouseenter', () => {
+    if (!startBtn.disabled) {
+        startBtn.style.backgroundColor = "#6C8CFF"; // 
+    }
+});
+
+// Додавання обробника події для стану normal кнопки
+startBtn.addEventListener('mouseleave', () => {
+    if (!startBtn.disabled) {
+        startBtn.style.backgroundColor = "#4E75FF"; // 
+    }
+});
+
+
+// Функція для оновлення таймера
 function updateTimer() {
     const currentTime = new Date().getTime();
     const difference = userSelectedDate - currentTime;
@@ -56,34 +106,38 @@ function updateTimer() {
                 number.textContent = "00";
             });
         }
+        setButtonState("disable");
         return;
     }
 
     const { days, hours, minutes, seconds } = convertMs(difference);
+
+    value[0].textContent = padZero(days);
+    value[1].textContent = padZero(hours);
+    value[2].textContent = padZero(minutes);
+    value[3].textContent = padZero(seconds);
 }
 
+// Функція для додавання нуля до чисел
 function padZero(num) {
     return String(num).padStart(2, '0');
 }
 
+// Функція для перетворення мілісекунд в об'єкт з часом
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minuates
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
+
 
 
 
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,11 @@
-console.log('Snackbar');
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitForm)
+
+function submitForm(event) {
+    event.preventDefault();
+    
+}
