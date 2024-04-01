e286036fefd451eb70fd3d5e7ad09189dd6d06ef
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-04-01 20:25:35 +0000

    Deploying to gh-pages from @ Vale4ka33/goit-js-hw-10@9083a930dfb33b5e23648ecdef6f131c6f0f8de9 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */console.log("Snackbar");
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-77e16229.js";const e=document.querySelector(".form");e.addEventListener("submit",o);function o(t){t.preventDefault()}
 //# sourceMappingURL=commonHelpers2.js.map
